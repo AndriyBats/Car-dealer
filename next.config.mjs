695162761a6cce67df92d65a0ecfc3b8bccb23b8
AndriyBats <84588992+AndriyBats@users.noreply.github.com>
2024-08-21 13:36:10 +0300

    next commit

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ const nextConfig = {
   env: {
     BASE_URL: process.env.REACT_APP_BASE_URL,
   },
+  output: 'export',
+  assetPrefix: 'https://AndriyBats.github.io/Car-dealer/',
 };
 
 export default nextConfig;
